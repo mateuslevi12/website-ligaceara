6b7cbce604700944f7e56a15e3df58c5616309ff
mateuslevi12 <mateuslevialencar32@gmail.com>
2024-11-27 22:27:58 -0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +9,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    port: 3000, // Define a porta para ambiente de desenvolvimento (se necessário)
+    host: true, // Permite acesso via IP ou domínio
+  },
+  preview: {
+    port: 4173, // Porta padrão usada pelo `vite preview`
+    host: true, // Permite acesso externo no ambiente de produção
+  },
+});
