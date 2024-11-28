04f362da3936dfde61bcfb39581d0ea4a3cfc653
mateuslevi12 <mateuslevialencar32@gmail.com>
2024-11-27 22:34:19 -0300

    voltando como era

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
@@ -9,12 +9,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    port: 3000,
-    host: true,
-  },
-  preview: {
-    port: 8080, // Alterando para uma porta padrão mais comum
-    host: true, // Permite acesso externo
-  },
-});
+})
