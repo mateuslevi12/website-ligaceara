e7a54983805653d4b35914093537bf1d565e24e9
mateuslevi12 <mateuslevialencar32@gmail.com>
2024-11-27 23:02:55 -0300

    build

--- src/data/patrocinios.ts
+++ src/data/patrocinios.ts
@@ -1,16 +1,16 @@
 export const patrocinioImages = [
-    "/public/assets/patrocinios/Ágil Contabilidade.avif",
-    "/public/assets/patrocinios/BOSCO.avif",
-    "/public/assets/patrocinios/diadora.avif",
-    "/public/assets/patrocinios/fit4.avif",
-    "/public/assets/patrocinios/futscout.avif",
-    "/public/assets/patrocinios/JOABY TROFEU.avif",
-    "/public/assets/patrocinios/normatel.avif",
-    "/public/assets/patrocinios/PRIMOS MOTOPEÇAS 01.avif",
-    "/public/assets/patrocinios/STANDARTT STT.avif",
-    "/public/assets/patrocinios/talissa-madeiras.avif",
-    "/public/assets/patrocinios/teles-odonto.avif",
-    "/public/assets/patrocinios/tijuca-alimentos.avif",
-    "/public/assets/patrocinios/WinnerCE - Logo.avif"
+    "/assets/patrocinios/Ágil Contabilidade.avif",
+    "/assets/patrocinios/BOSCO.avif",
+    "/assets/patrocinios/diadora.avif",
+    "/assets/patrocinios/fit4.avif",
+    "/assets/patrocinios/futscout.avif",
+    "/assets/patrocinios/JOABY TROFEU.avif",
+    "/assets/patrocinios/normatel.avif",
+    "/assets/patrocinios/PRIMOS MOTOPEÇAS 01.avif",
+    "/assets/patrocinios/STANDARTT STT.avif",
+    "/assets/patrocinios/talissa-madeiras.avif",
+    "/assets/patrocinios/teles-odonto.avif",
+    "/assets/patrocinios/tijuca-alimentos.avif",
+    "/assets/patrocinios/WinnerCE - Logo.avif"
   ];
   
